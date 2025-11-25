a3c5022eff44d4a6ca580a35cd1b5644ca58c9ec
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-11-25 22:22:22 +0900

    [add]レコードモデルの入力

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,9 @@
 class Record < ApplicationRecord
+    belongs_to :user
+    belongs_to :image
+    belongs_to :category
+
+    enum status: { draft: 0, published: 1 }
+
+    validates :star_rating,  inclusion: { in: 1..5 }
 end
