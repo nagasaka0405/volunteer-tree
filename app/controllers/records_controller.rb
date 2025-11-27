d9e3739d2b526fca84542d47d6d4bec087e22b9b
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-11-27 22:34:08 +0900

    [add]投稿フォームの保存ルートとコントローラ作成

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,4 +2,14 @@ class RecordsController < ApplicationController
   def new
     @record = Record.new
   end
+
+  def create
+    record = Record.new(post_params)
+    record.save
+    redirect_to '/top'
+  end
+  private
+  def record_params
+    params.require(:record).permit(:location, :text)
+  end
 end
