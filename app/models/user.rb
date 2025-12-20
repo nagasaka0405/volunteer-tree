56a19ce7e4b39418e85ff98c5b951b37355bc048
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-12-20 09:31:37 +0900

    [fix]フォロー・フォロワーモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   has_many :records, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :following_users, through: :follower, source: :followed
-  has_many :follower_users, through: :followed, source: :follower
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_users, through: :followers, source: :followed
+  has_many :follower_users, through: :followeds, source: :follower
 
   has_one_attached :profile_image
 
