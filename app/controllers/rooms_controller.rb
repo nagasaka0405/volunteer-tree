3ff694a98d6c4be1068351cb1d97ca83cecf682f
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-12-30 08:06:14 +0900

    [add]roomコントローラ作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,4 +7,16 @@ class RoomsController < ApplicationController
         @another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
         redirect_to room_path(@room)
     end
+
+    def show
+        @room = Room.find(params[:id])
+        if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+            @messages = @room.messages
+            @message = Message.new
+            @entries = @room.entries
+            @my_account = current_user.id
+        else
+            redirect_back(fallback_location: root_path)
+        end
+    end
 end
