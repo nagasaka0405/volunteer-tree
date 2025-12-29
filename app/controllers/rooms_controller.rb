e4962f884be0e2f5d0ec1a3cca7e8f55fd6de04b
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-12-29 18:43:30 +0900

    [add]メッセージフォーム作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,10 @@
 class RoomsController < ApplicationController
+    before_action:authenticate_user!
+
+    def create
+        @room = Room.create(user_id: current_user.id)
+        @current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
+        @another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
+        redirect_to room_path(@room)
+    end
 end
