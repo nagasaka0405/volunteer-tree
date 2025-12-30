66a8fce64cd62373a4d76ee98e9a096c236a3dd0
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-12-30 22:03:51 +0900

    [fix]DM機能のエラー修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,16 @@
 class MessagesController < ApplicationController
-    before_action:authenticate_user!, :only => [:create]
-    
+    before_action :authenticate_user!, only: [:create]
+
     def create
        @message = Message.new(message_params)
        @message.user_id = current_user.id
+
+       # room_idが空の場合はエラー回避
+       if @message.room_id.blank?
+        redirect_back(fallback_location: root_path, alert: "ルーム情報がありません")
+        return
+       end
+
        @room = Room.find(@message.room_id)
 
        if @message.save
