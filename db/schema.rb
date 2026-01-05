2b1e290d27f48e57eefc5bcde5fe0b35e041bc32
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2026-01-05 22:14:17 +0900

    [fix]roomからユーザidカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_26_130925) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_05_131234) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -124,10 +124,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_26_130925) do
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -156,5 +154,4 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_26_130925) do
   add_foreign_key "records", "users"
   add_foreign_key "relationships", "users", column: "followed_id"
   add_foreign_key "relationships", "users", column: "follower_id"
-  add_foreign_key "rooms", "users"
 end
