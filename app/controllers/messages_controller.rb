6a9c5df165bf1ce9c9f9f829b94bd2ddbaa0a9a2
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2026-01-06 22:28:39 +0900

    [fix]roomsにmessagesをネスト

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,27 +2,21 @@ class MessagesController < ApplicationController
     before_action :authenticate_user!, only: [:create]
 
     def create
-       @message = Message.new(message_params)
+       @room = Room.find(params[:room_id])
+       @message = @room.messages.new(message_params)
        @message.user_id = current_user.id
 
-       # room_idが空の場合はエラー回避
-       if @message.room_id.blank?
-        redirect_back(fallback_location: root_path, alert: "ルーム情報がありません")
-        return
-       end
-
-       @room = Room.find(@message.room_id)
-
        if @message.save
-           redirect_to room_path(@message.room_id)
+           redirect_to room_path(@room)
        else
            @messages = @room.messages.includes(:user)
+           @entries = @room.entries
            render "rooms/show"
        end
     end
 
     private
     def message_params
-       params.require(:message).permit(:room_id, :message)
+       params.require(:message).permit(:message)
     end
 end
