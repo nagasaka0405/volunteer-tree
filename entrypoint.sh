5b385b65cda95ae606bf4659d365bc214c81404f
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2026-06-28 09:51:30 +0900

    [entrypointでmigrate]

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
+# 本番起動時だけ migrate を実行
+if [ "$RENDER" = "true" ]; then
+  echo "Running migrations..."
+  bundle exec rails db:migrate
+fi
+
 echo "Starting server..."
 bundle exec rails server -p ${PORT:-3000} -e production
