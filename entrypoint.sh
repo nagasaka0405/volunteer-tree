f4bd8fe69debf8018f1b5d0e449a2fb480dccf8d
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2026-07-09 20:03:13 +0900

    [fix]entrypoint再修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Running migrations and seeds..."
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:seed
-
 echo "Starting server..."
 bundle exec rails server -p ${PORT:-3000} -e production
