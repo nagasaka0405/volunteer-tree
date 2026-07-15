0fa19a5190cb5714f22f083044c3bf63913d7485
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2026-07-15 19:29:18 +0900

    [fix]seeds.rb

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Preparing database..."
+bundle exec rails db:prepare
+bundle exec rails db:seed
+
 echo "Starting server..."
 bundle exec rails server -p ${PORT:-3000} -e production
